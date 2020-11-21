0a0bafad6f5b94b4d06c022710e71f4577650c99
okrutnik123 <jan.nowacki@adres.pl>
2020-11-21 14:52:55 +0100

    Added getApplicationPath()

--- launch.cpp
+++ launch.cpp
@@ -5,18 +5,36 @@
 #endif
 #include <iostream>
 #include <string.h>
+#include <limits.h>
 
 using namespace std;
+
+string exename;
+string args;
+
+string getApplicationDirectory() 
+{
+    char result[ PATH_MAX ];
+    ssize_t count = readlink( "/proc/self/exe", result, PATH_MAX );
+    std::string appPath = std::string( result, (count > 0) ? count : 0 );
+
+    std::size_t found = appPath.find_last_of("/\\");
+    return appPath.substr(0,found);
+}
 
 int main(int argc,char* argv[]) 
 {
     //args to std::string
-    std::string args= "";
-    for (int i=0;i<argc;i++) args.append(std::string(argv[i]).append(" "));
+    args= "";
+    exename = argv[0];
+    for (int i=1;i<argc;i++) args.append(std::string(argv[i]).append(" "));
+
     //print name
     cout << "Image Compressor v.1.0" << endl;
+
     //concat command and run
-    string total(string("java -jar ") + args);
+    string total(string("java -jar \"") + getApplicationDirectory() + "/" + exename + "\"" + args);
     system(total.c_str());
+
     return 0;
 }
